a036ac2a11ac7e22fcc6f4713861ed8a91d682a5
maxhurst <maxhurst@seattleacademy.org>
2024-01-05 10:45:42 -0800

    first

---
+++ script.js
@@ -0,0 +1,36 @@
+const box = document.getElementById('box')
+
+box.addEventListener('click', changeColor);
+
+
+
+function changeColor() {
+    const R = Math.floor(Math.random()*255);   
+    const G = Math.floor(Math.random()*255);
+    const B = Math.floor(Math.random()*255);
+
+
+box.style.backgroundColor = `rgb(${R},${G},${B})`;
+}
+
+
+
+
+
+const circle = document.getElementById('box')
+
+
+
+
+circle.addEventListener('click', changeColor);
+
+
+
+function changeColor() {
+    const R = Math.floor(Math.random()*255);   
+    const G = Math.floor(Math.random()*255);
+    const B = Math.floor(Math.random()*255);
+
+
+circle.style.backgroundColor = `rgb(${R},${G},${B})`;
+}
